1c84ebd24004ee6925015f2194427d831aad5a10
Louis <louis993546@gmail.com>
2024-05-14 00:35:02 +0200

    Make it look nicer

--- app/src/main/java/de/berlindroid/bingo/MainActivity.kt
+++ app/src/main/java/de/berlindroid/bingo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -31,8 +33,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import dev.shreyaspatil.capturable.capturable
 import dev.shreyaspatil.capturable.controller.rememberCaptureController
 import kotlinx.coroutines.launch
@@ -101,6 +105,21 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun Header() {
+    Row(
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.avatar),
+            contentDescription = "",
+            modifier = Modifier.height(128.dp)
+        )
+        Spacer(modifier = Modifier.padding(16.dp))
+        Text("Google I/O 2024 Bingo Card", fontSize = 24.sp)
+    }
+}
 
 @Composable
 fun Content(
@@ -110,12 +129,12 @@ fun Content(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .padding(32.dp)
+            .aspectRatio(0.7070707f) // ISO216 A-series
     ) {
-        Text("Berlindroid Google I/O 2024 Bingo Card") // TODO logo
+        Header()
         Spacer(modifier = Modifier.padding(16.dp))
         Bingo(
             data = bingoData,
-            modifier = Modifier.aspectRatio(0.7070707f) // ISO216 A-series
         )
     }
 }
